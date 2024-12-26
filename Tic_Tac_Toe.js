780140e3c2373470b16e7976f5713a1806dbc78c
biswa18121997 <biswajitshrm6@gmail.com>
2024-12-26 17:50:33 +0530

    first commit

---
+++ Tic_Tac_Toe.js
@@ -0,0 +1,6 @@
+let boxes=[];
+
+
+for(let x=0;x<9;x++){
+    boxes[x]=document.
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Tic_Tac_Toe.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
